494d6118b917533df0c7dc2d90432e25c76d6972
TheFoxDecoder <526473+gb96@users.noreply.github.com>
2019-10-02 05:13:09 +0530

    a few change of fizz_buzz py || swift

--- fizz-buzz.swift
+++ fizz-buzz.swift
@@ -1,5 +1,9 @@
- //for example
-let number = [1,2,3,4,5]
+/* Algorithm:-  Fizz and Buzz refer to any number that's a multiple of 3 and 5 respectively. 
+                In other words, if a number is divisible by 3, it is substituted with fizz; 
+                if a number is divisible by 5, it is substituted with buzz. 
+                If a number is simultaneously a multiple of 3 AND 5, the number is replaced with "fizz buzz." 
+*/
+ let number = [1,2,3,4,5]
 // here 3 is fizz and 5 is buzz
 
 for num in number { 
