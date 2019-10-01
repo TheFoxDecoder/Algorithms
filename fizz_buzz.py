d5ba2178687c6b31d55dd08826e0bbe0e9a8dfe5
TheFoxDecoder <526473+gb96@users.noreply.github.com>
2019-10-02 05:05:59 +0530

    fizz_buzz done

---
+++ fizz_buzz.py
@@ -0,0 +1,19 @@
+'''Algorithm:-  Fizz and Buzz refer to any number that's a multiple of 3 and 5 respectively. 
+                In other words, if a number is divisible by 3, it is substituted with fizz; 
+                if a number is divisible by 5, it is substituted with buzz. 
+                If a number is simultaneously a multiple of 3 AND 5, the number is replaced with "fizz buzz." '''
+import array as arr
+
+#number = arr.array('i'[1,2,3,4,5,6,7,8,9,10])
+'''3 is fizz 5 is buzz'''
+number = [1,2,3,4,5,6,7,8,9,10]
+
+for num in number:
+    if (num % 3 == 0):
+        print("%d Fizz"%num)
+    elif (num %5 == 0):
+        print("%d Buzz"%num)
+    else :
+        print(num)    
+
+
