153c1ca43ac2497ef2a79a066a23f06daf2b0c9c
TheFoxDecoder <526473+gb96@users.noreply.github.com>
2019-11-08 03:14:39 +0530

    not yet

---
+++ Euler's_identity.py
@@ -0,0 +1,12 @@
+'''
+here is the simple example of Euler's identity algorithm
+
+'''
+import math
+import cmath 
+
+e = math.e
+i = cmath.sqrt(-1)
+tt= float(math.pow(i,math.pi))
+sum = math.pow(e,tt) + 1 
+print(tt)
