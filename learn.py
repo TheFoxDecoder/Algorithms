45b948a6085977fdbd9751e2b28f6ddbb51dadbd
TheFoxDecoder <526473+gb96@users.noreply.github.com>
2020-02-14 02:27:04 +0530

    algorithamic push

---
+++ learn.py
@@ -0,0 +1,10 @@
+from array import *
+
+arr = array('i',[3,4,5,6])
+x=len(arr)
+y = (x - 1)
+z =(x-y)
+for i in range(0,len(arr)):
+    print('Number {} = {}'.format(i+1,i))
+
+#print('0 =>',arr[0])
